96d32a6cf885d31dd8b912fdd026b65ca5350a75
mao2116 <78520987+mao2116@users.noreply.github.com>
2021-03-05 13:01:59 +0600

    Create bscpkg.sh

---
+++ bscpkg.sh
@@ -0,0 +1,75 @@
+apt update
+
+apt upgrade
+
+termux-setup-storage
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install git -y
+
+pkg install figlet -y
+
+pkg install cmatrix -y
+
+pkg install toilet -y
+
+pkg install nano -y
+
+pkg install php -y
+
+pip install astroid 
+pip install autopep8
+pip install certifi
+pip install chardet
+pip install colorama 
+pip install future
+pip install idna 
+pip install isort 
+pip install lazy-object-proxy
+pip install lolcat 
+pip install mccabe  
+pip install Pillow 
+pip install pilo 
+pip install pip
+pip install pycodestyle 
+pip install pyfiglet
+
+pip install pylint
+
+pip install requests 
+
+pip install setuptools
+
+pip install six
+
+pip install termcolor
+
+pip install toml
+
+pip install urllib3
+
+
+
+
+pip install wheel
+
+pip install wrapt
+
+pip install youtube-dl
+
+apt install ruby
+
+apt install openssh
+
+apt install wget
+
+apt install curl
+
+apt install proot
+
+
+
+#Remake By Mao2116
